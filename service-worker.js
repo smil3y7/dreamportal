1af632bbce81d98869542eaae5f2aff675c77fc5
smil3y7 <37001849+smil3y7@users.noreply.github.com>
2025-11-16 17:51:51 +0100

    Update service-worker.js
    v5

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
 const CACHE = "dreamportal-v1";
-const FILES = ["/", "/index.html", "/css/style.css", "/js/main.js", "/manifest.json", "/icons/192.png"];
+const FILES = [
+  "/", "/index.html", "/css/style.css",
+  "/js/app.js", "/js/db.js", "/js/data.js", "/js/render.js", "/js/ui.js",
+  "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"
+];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
